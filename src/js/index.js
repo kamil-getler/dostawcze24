833017fbc394a83093e0df307b7f28dc4668a63f
KamilGetler <kamilgetler@gmail.com>
2022-05-10 00:29:54 +0200

    ResponsiveMenu

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,9 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const menuHamburger4 = document.getElementById('menu-icon-wrapper-4');
+
+menuHamburger4.addEventListener('click', () => {
+    menuHamburger4.classList.toggle('open');
+}, false);
