1316d4965b5001b98d50cf242a54c8edb060b64f
KamilGetler <kamilgetler@gmail.com>
2022-05-10 10:13:32 +0200

    newLokalizacja

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,16 @@ console.log('HELLO 🚀')
 
 const menuHamburger4 = document.getElementById('menu-icon-wrapper-4');
 
+const activehamburger = document.getElementById('sidebar__active');
+const activelokalizacja = document.getElementById('active__lokalizacja');
+
 menuHamburger4.addEventListener('click', () => {
     menuHamburger4.classList.toggle('open');
+    activehamburger.classList.toggle('active')
+
+}, false);
+const activelok = document.getElementById('nav__active');
+activelokalizacja.addEventListener('click', () => {
+
+    activelok.classList.toggle('active__loka')
 }, false);
